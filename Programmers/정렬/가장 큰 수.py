45857291e008a19007b64ac40497f305673c571b
parkjh4550 <37648792+parkjh4550@users.noreply.github.com>
2021-09-21 05:51:14 +0900

    Add files via upload

---
+++ Programmers/정렬/가장 큰 수.py
@@ -0,0 +1,30 @@
+def solution(numbers):
+    if len(set(numbers)) == 1 and numbers[0] == 0:
+        return '0'
+    def fill_number(number, length):
+        # 자릿수 변경하는 함수
+        filled_number = ''
+        while True:
+            filled_number += number
+            if len(filled_number) > length:
+                break
+        return filled_number[:length]
+
+    max_length = len('1000')
+    # filled_numbers = [[ 자릿수 변경 후 숫자, 자릿 수 변경 전 숫자], ... ]
+    filled_numbers = [[int(fill_number(str(n), max_length)), n] for n in numbers]
+    filled_numbers = sorted(filled_numbers, reverse=True, key=lambda x: x[0])
+    answer = ''.join([str(n[1]) for n in filled_numbers])
+
+    return answer
+
+if __name__ == '__main__':
+    print(solution([6, 10, 2]), "6210")
+    print(solution([42, 33, 76]), "764233")
+    print(solution([4, 40]), "440")
+    print(solution([3, 30, 34, 5, 9]), "9534330")
+    print(solution([40, 403]), '40403')
+    print(solution([0, 0, 0]), '0')
+    print(solution([383, 38]), "38383")
+    print(solution([838, 83]), "83883")
+    print(solution([11, 11]), "1111")
